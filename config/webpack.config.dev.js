73b9ba30fc8903ef333887067a5d91f249062a8d
Mark <mark@remarkablemark.org>
2018-05-19 16:40:34 -0400

    chore: add `raw-loader` so text can be imported

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -191,6 +191,12 @@ module.exports = {
             ],
           },
 
+          // Load raw text.
+          {
+            test: /\.txt$/,
+            loader: 'raw-loader',
+          },
+
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -206,6 +206,12 @@ module.exports = {
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
 
+          // Load raw text.
+          {
+            test: /\.txt$/,
+            loader: 'raw-loader',
+          },
+
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
